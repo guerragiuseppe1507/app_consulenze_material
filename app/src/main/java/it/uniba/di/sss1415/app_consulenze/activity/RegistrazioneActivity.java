30b3bda19d92de357e2dd3482ef17da0c0322df4
guerragiuseppe1507 <Evangelion92>
2015-06-21 11:49:22 +0200

    Signup e signin funzionanti

--- app/src/main/java/it/uniba/di/sss1415/app_consulenze/activity/RegistrazioneActivity.java
+++ app/src/main/java/it/uniba/di/sss1415/app_consulenze/activity/RegistrazioneActivity.java
@@ -1,6 +1,7 @@
 package it.uniba.di.sss1415.app_consulenze.activity;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
@@ -10,6 +11,7 @@ import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.Toast;
 
 import java.util.Random;
 
@@ -23,20 +25,22 @@ public class RegistrazioneActivity extends Activity {
 
     private Connection conn;
     private String parametriServer;
+    private UserRegisterTask mRegTask = null;
 
-    private static final String TIPO_ELEMENTO = "appuntamenti";
-    private static final String ACCESSO = "read";
+    private static final String TIPO_ELEMENTO = "registrazione";
+    private static final String ACCESSO = "write";
 
 
     public EditText editEmail;
     private EditText editAnnoIscrizione;
     private EditText editNumero;
     private Spinner spinnerProvince;
+    private Button regButton;
+    private Button loginButton;
 
     //per la generazione della password
     private static final String _CHAR = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
     private static final int RANDOM_STR_LENGTH = 16;
-    private String password; //password dda generare random
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -61,7 +65,15 @@ public class RegistrazioneActivity extends Activity {
         // Apply the adapter to the spinner
         spinnerProvince.setAdapter(adapter);
 
-        Button loginButton = (Button) findViewById(R.id.link_to_login);
+        regButton = (Button) findViewById(R.id.btnRegister);
+        regButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                registrazioneUtente();
+            }
+        });
+
+        loginButton = (Button) findViewById(R.id.link_to_login);
         loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -84,9 +96,10 @@ public class RegistrazioneActivity extends Activity {
         return output;
     }
 
-    //prende i dati inseriti e genera la stringa di parametri
-    public void prendiDati() {
-        password = generaPass();//generazione della password
+    //evento per registrare l'utente
+    public void registrazioneUtente(){
+        //recupero tutti i dati inseriti dall'utente e genero la stringa dei parametri da inviare memorizzando tutto nei membri interni
+        String password = generaPass();//generazione della password
 
         //recuperiamo i dati inseriti dall'utente
         String email = editEmail.getText().toString();
@@ -94,37 +107,34 @@ public class RegistrazioneActivity extends Activity {
         String numero = editNumero.getText().toString();
         String provincia = spinnerProvince.getSelectedItem().toString();
 
-        //genero la stringa di parametri
-        conn.setParametri(TIPO_ELEMENTO, ACCESSO, email, numero, anno, provincia, password);
-        Log.i("PARAMETRI = ", conn.getParametri());
-    }
-
-    //evento per registrare l'utente
-    public void registrazioneUtente(View v){
-        //recupero tutti i dati inseriti dall'utente e genero la stringa dei parametri da inviare memorizzando tutto nei membri interni
-        prendiDati();
-        inviaRichiesta();
-
-        mostraHome();
+        mRegTask = new UserRegisterTask(email, numero, anno, provincia, password);
+        mRegTask.execute();
 
     }
 
     public class UserRegisterTask extends AsyncTask<String, Void, String> {
 
         private final String mEmail;
+        private final String mNumero;
+        private final String mAnno;
+        private final String mProvincia;
         private final String mPassword;
 
 
 
-        UserRegisterTask(String email, String password) {
+        UserRegisterTask(String email, String numero, String anno , String provincia, String password) {
             mEmail = email;
+            mNumero = numero;
+            mAnno = anno;
+            mProvincia = provincia;
             mPassword = password;
         }
 
         @Override
         protected String doInBackground(String... params) {
 
-            conn.setParametri(TIPO_ELEMENTO, ACCESSO, mEmail, mPassword);
+            conn.setParametri(TIPO_ELEMENTO, ACCESSO, mEmail, mNumero, mAnno, mProvincia, mPassword);
+            Log.i("PARAMETRI = ", conn.getParametri());
 
             return conn.newConnect();
 
@@ -143,20 +153,19 @@ public class RegistrazioneActivity extends Activity {
 
                 creaMessaggio(getApplicationContext().getResources().getString(R.string.fail_login));
             } else {
-                //datiUser = new DatiUtente(result);
-                datiUser = new DatiUtente(result);
+
                 toMain();
-                creaMessaggio(getApplicationContext().getResources().getString(R.string.success_login));
+                creaMessaggio(getApplicationContext().getResources().getString(R.string.success_register));
 
             }
 
-            mAuthTask = null;
+            mRegTask = null;
 
         }
 
         @Override
         protected void onCancelled() {
-            mAuthTask = null;
+            mRegTask = null;
         }
     }
 
@@ -165,6 +174,15 @@ public class RegistrazioneActivity extends Activity {
         startActivity(intent);
     }
 
+    public void toMain(){
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
 
+    public void creaMessaggio(CharSequence message){
+        Context context = getApplicationContext();
+        Toast toastMessage = Toast.makeText(context, message, Toast.LENGTH_LONG);
+        toastMessage.show();
+    }
 
 }
