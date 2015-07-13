b3fa130ddf7dfcb706c5af46f43c749a04383c17
guerragiuseppe1507 <Evangelion92>
2015-07-13 13:10:14 +0200

    signleton e appuntamenti in home

--- app/src/main/java/it/uniba/di/sss1415/app_consulenze/activity/MieDispFragment.java
+++ app/src/main/java/it/uniba/di/sss1415/app_consulenze/activity/MieDispFragment.java
@@ -84,7 +84,7 @@ public class MieDispFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View view = inflater.inflate(R.layout.fragment_miedisp, container, false);
+        View view = inflater.inflate(R.layout.fragment_carlo, container, false);
         layoutManager = new LinearLayoutManager(getActivity());
         layoutManager.setOrientation(LinearLayoutManager.VERTICAL);
         recyclerView = (RecyclerView) view.findViewById(R.id.mieDisp_recycler);
