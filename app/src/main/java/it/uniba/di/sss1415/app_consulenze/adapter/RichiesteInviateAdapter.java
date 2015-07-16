3a33f16a760b4fefccc41ddb61ef5db47e5b5029
ArViii <valerio0193@hotmail.it>
2015-07-17 00:47:10 +0200

    Inizio sviluppo logica fragment richieste

---
+++ app/src/main/java/it/uniba/di/sss1415/app_consulenze/adapter/RichiesteInviateAdapter.java
@@ -0,0 +1,15 @@
+package it.uniba.di.sss1415.app_consulenze.adapter;
+
+/**
+ * Created by Valerio on 17/07/2015.
+ */
+public class RichiesteInviateAdapter {
+
+    /*TODO Per ognuno dei due fragment inviate/ricevute bisogna:
+        1)Implementare l'adapter
+        2)Mettere la recycleView nei layout
+        3)Creare gli item cardview
+        4)Creare l'entità "richiesta" per gestire i dati
+
+     */
+}
---
+++ app/src/main/java/it/uniba/di/sss1415/app_consulenze/adapter/RichiesteRicevuteAdapter.java
@@ -0,0 +1,7 @@
+package it.uniba.di.sss1415.app_consulenze.adapter;
+
+/**
+ * Created by Valerio on 17/07/2015.
+ */
+public class RichiesteRicevuteAdapter {
+}
