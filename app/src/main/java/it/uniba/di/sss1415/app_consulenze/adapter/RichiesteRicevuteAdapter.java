43f55d480c680a80c5b463da3accc65612fd0c33
ArViii <valerio0193@hotmail.it>
2015-07-17 12:53:08 +0200

    Fragment rechieste inviate e ricevute quasi complete

--- app/src/main/java/it/uniba/di/sss1415/app_consulenze/adapter/RichiesteRicevuteAdapter.java
+++ app/src/main/java/it/uniba/di/sss1415/app_consulenze/adapter/RichiesteRicevuteAdapter.java
@@ -1,7 +1,87 @@
 package it.uniba.di.sss1415.app_consulenze.adapter;
 
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+import app_consulenze_material.R;
+import it.uniba.di.sss1415.app_consulenze.istances.RichiesteInviate;
+import it.uniba.di.sss1415.app_consulenze.istances.RichiesteRicevute;
+
 /**
  * Created by Valerio on 17/07/2015.
  */
-public class RichiesteRicevuteAdapter {
+public class RichiesteRicevuteAdapter extends  RecyclerView.Adapter<RichiesteRicevuteAdapter.RichiesteRicevuteHolder> {
+
+    private Context context;
+    private ArrayList<RichiesteRicevute> items;
+
+    public RichiesteRicevuteAdapter(Context context, ArrayList<RichiesteRicevute> items) {
+        this.context = context;
+        this.items = items;
+    }
+
+    // Create new views (invoked by the layrout manage)
+    @Override
+    public RichiesteRicevuteHolder onCreateViewHolder (ViewGroup parent,int viewType){
+        View view = LayoutInflater.from(context).inflate(R.layout.item_richieste_ricevute, parent, false);
+        RichiesteRicevuteHolder viewHolder = new RichiesteRicevuteHolder(context, view);
+        return viewHolder;
+    }
+
+    // Replace the contents of a view (invoked by the layout manager)
+    @Override
+    public void onBindViewHolder(RichiesteRicevuteHolder viewHolder, int position) {
+        RichiesteRicevute request = items.get(position);
+        viewHolder.tvDataRequest.setText(request.getData());
+        viewHolder.tvOraInizioRequest.setText(request.getOraInizio());
+        viewHolder.tvOraFineRequest.setText(request.getOraFine());
+        viewHolder.tvInterventoRequest.setText(request.getIntervento());
+        viewHolder.tvDottoreRequest.setText(request.getDottore());
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return items.size();
+    }
+
+    public class RichiesteRicevuteHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
+
+        private Context context;
+        public TextView tvDataRequest;
+        public TextView tvOraInizioRequest;
+        public TextView tvOraFineRequest;
+        public TextView tvInterventoRequest;
+        public TextView tvDottoreRequest;
+
+
+
+        public RichiesteRicevuteHolder(Context context, View itemView) {
+            super(itemView);
+            this.context = context;
+            tvDataRequest = (TextView) itemView.findViewById(R.id.tvDataRequestReceived);
+            tvOraInizioRequest = (TextView) itemView.findViewById(R.id.tvOraInizioRequestReceived);
+            tvOraFineRequest = (TextView) itemView.findViewById(R.id.tvOraFineRequestReceived);
+            tvInterventoRequest = (TextView) itemView.findViewById(R.id.tvInterventoRequestReceived);
+            tvDottoreRequest = (TextView) itemView.findViewById(R.id.tvDottoreRequestReceived);
+
+            itemView.setOnClickListener(this);
+        }
+
+        @Override
+        public void onClick(View v) {
+            Toast.makeText(context, tvDataRequest.getText().toString(), Toast.LENGTH_SHORT).show();
+        }
+
+    }
+
+
+
 }
--- app/src/main/java/it/uniba/di/sss1415/app_consulenze/fragment/RichiesteRicevuteFragment.java
+++ app/src/main/java/it/uniba/di/sss1415/app_consulenze/fragment/RichiesteRicevuteFragment.java
@@ -1,18 +1,51 @@
 package it.uniba.di.sss1415.app_consulenze.fragment;
 
+import android.content.Context;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
+
+import org.json.JSONException;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashMap;
 
 import app_consulenze_material.R;
+import it.uniba.di.sss1415.app_consulenze.adapter.RichiesteInviateAdapter;
+import it.uniba.di.sss1415.app_consulenze.adapter.RichiesteRicevuteAdapter;
+import it.uniba.di.sss1415.app_consulenze.istances.RichiesteInviate;
+import it.uniba.di.sss1415.app_consulenze.istances.RichiesteRicevute;
+import it.uniba.di.sss1415.app_consulenze.util.Connection;
+import it.uniba.di.sss1415.app_consulenze.util.JsonHandler;
+import it.uniba.di.sss1415.app_consulenze.util.ServerResponseDataSorter;
+import it.uniba.di.sss1415.app_consulenze.util.ToastMsgs;
 
 /**
  * Created by Valerio on 16/07/2015.
  */
 public class RichiesteRicevuteFragment extends Fragment {
+    private static final String NOME_RICHIESTA = "richiesteValutare";
+    private static final String TIPO_ACCESSO = "read";
     public static final String ARG_PAGE = "ARG_PAGE";
+
+    private RichiesteRicevuteAdapter richiesteRicevuteAdapter;
+    private LinearLayoutManager layoutManager;
+    private RecyclerView recyclerView;
+
+    private ShowReceivedRequestTask dispTask = null;
+    private Connection conn;
+
+    ArrayList<RichiesteRicevute> requests;
 
     private int mPage;
 
@@ -28,6 +61,36 @@ public class RichiesteRicevuteFragment extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         mPage = getArguments().getInt(ARG_PAGE);
+
+        conn = new Connection(getActivity().getApplicationContext().getResources().getString(R.string.serverQuery));
+        dispTask = new ShowReceivedRequestTask();
+        dispTask.execute();
+    }
+
+    private void createAndPopulateCountriesArray(ArrayList<HashMap<String,String>> res) {
+
+        Date d = new Date();
+        SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
+
+        ServerResponseDataSorter.sort(res);
+
+        requests = new ArrayList<RichiesteRicevute>();
+        for(int i = 0; i < res.size(); i++) {
+
+            HashMap<String,String> temp = res.get(i);
+
+            try {
+                if(formatter.parse(temp.get("data")).getTime() > d.getTime()){
+
+                    requests.add(new RichiesteRicevute(temp.get("data"), temp.get("oraInizio"), temp.get("oraFine"),
+                            temp.get("intervento"), temp.get("dottore")));
+
+                }
+            } catch (ParseException e) {
+                e.printStackTrace();
+            }
+
+        }
     }
 
     @Override
@@ -35,7 +98,79 @@ public class RichiesteRicevuteFragment extends Fragment {
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_richieste_ricevute, container, false);
 
+        layoutManager = new LinearLayoutManager(getActivity());
+        layoutManager.setOrientation(LinearLayoutManager.VERTICAL);
+        recyclerView = (RecyclerView) view.findViewById(R.id.richieste_ricevute_recycler);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
 
         return view;
+    }
+
+    private void setRecycler(){
+        richiesteRicevuteAdapter = new RichiesteRicevuteAdapter(getActivity(), requests);
+        recyclerView.setAdapter(richiesteRicevuteAdapter);
+    }
+
+    @Override
+    public void onDetach() {
+        super.onStop();
+        if(dispTask!=null)dispTask.cancel(true);
+    }
+
+    public class ShowReceivedRequestTask extends AsyncTask<String, Void, String> {
+
+        ShowReceivedRequestTask() {
+        }
+
+        @Override
+        protected String doInBackground(String... params) {
+
+            conn.setParametri(NOME_RICHIESTA, TIPO_ACCESSO);
+
+            return conn.newConnect();
+
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+
+            System.out.println(result);
+
+            ArrayList<HashMap<String,String>> listaRichieste;
+
+            if (result.equals(ToastMsgs.CONN_TIMEOUT)){
+
+                creaMessaggio(getActivity().getApplicationContext().getResources().getString(R.string.conn_timeout));
+            } else {
+                try {
+                    listaRichieste = JsonHandler.fromJsonToMapList(NOME_RICHIESTA, result);
+                    createAndPopulateCountriesArray(listaRichieste);
+
+                    setRecycler();
+
+                } catch (JSONException e) {
+                    creaMessaggio(ToastMsgs.JSON_TO_ARRAY_ERROR);
+                    e.printStackTrace();
+                }
+            }
+
+
+
+            dispTask = null;
+
+        }
+
+        @Override
+        protected void onCancelled() {
+            dispTask = null;
+        }
+    }
+
+    public void creaMessaggio(CharSequence message){
+        Context context = getActivity().getApplicationContext();
+        Toast toastMessage = Toast.makeText(context, message, Toast.LENGTH_LONG);
+        toastMessage.show();
     }
 }
