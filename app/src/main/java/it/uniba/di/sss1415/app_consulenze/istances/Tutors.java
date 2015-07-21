34ef0b8d16e98e3d164c4c258bb87868eaba0e00
PasenELMagnete <lacalamita.pasquale@gmail.com>
2015-07-21 20:30:51 +0200

    Tutor rating created part 1

---
+++ app/src/main/java/it/uniba/di/sss1415/app_consulenze/istances/Tutors.java
@@ -0,0 +1,25 @@
+package it.uniba.di.sss1415.app_consulenze.istances;
+
+/**
+ * Created by Pasen on 21/07/2015.
+ */
+public class Tutors {
+    private String nomeT;
+    private String cognomeT;
+    private String scoreT;
+
+
+    public Tutors(String nomeT, String cognomeT, String scoreT){
+
+        this.nomeT = nomeT;
+        this.cognomeT = cognomeT;
+        this.scoreT = scoreT;
+
+
+    }
+
+    public String getNome(){return this.nomeT;}
+    public String getCognome(){return this.cognomeT;}
+    public String getScore(){return this.scoreT;}
+
+}
