d28d9f47f653444d022d5c3abe7c3bfe40aeed90
unknown <ArViii>
2015-07-22 12:13:45 +0200

    tost msgs fix

--- app/src/main/java/it/uniba/di/sss1415/app_consulenze/util/ToastMsgs.java
+++ app/src/main/java/it/uniba/di/sss1415/app_consulenze/util/ToastMsgs.java
@@ -4,7 +4,7 @@ package it.uniba.di.sss1415.app_consulenze.util;
  * Created by Giuseppe on 07/07/2015.
  */
 public class ToastMsgs {
-    public static final String JSON_TO_ARRAY_ERROR = "Errore elaborazione risultati";
-    public static final String CONN_TIMEOUT = "timeout";
+    public static final String JSON_TO_ARRAY_ERROR = "Error Elaborating Results";
+    public static final String CONN_TIMEOUT = "Timeout";
     public static final String CONN_PROBLEM = "Connection Problem";
 }
